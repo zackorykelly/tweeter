3921a572d7980b0ea782d144cb7e21d15ede38d4
Zackory Kelly <zackkelly@live.ca>
2021-04-22 23:44:11 +0000

    Organize primary js code to improve readability

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,24 +4,30 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+//Loads all tweets from the database and passes the array for rendering
 const loadTweets = function() {
   $.get("/tweets", function(tweets) {
     renderTweets(tweets);
   });
 };
 
+//Escape function that tweets are put through to prevent script injection attacks
 const escape = function(str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 };
 
+//Takes in a single tweet element and pulls all the relevant data to create an html object to render
 const createTweetElement = function(tweet) {
   const name = tweet.user.name;
   const handle = tweet.user.handle;
   const avatar = tweet.user.avatars;
   const message = tweet.content.text;
+  //timeago converts numeral date format into 'x days ago' etc.
   const time = timeago.format(tweet.created_at);
+
+  //String literal tweet format
   const $tweet = $(`
   <article>
     <header>
@@ -40,19 +46,22 @@ const createTweetElement = function(tweet) {
     </footer>
   </article>
   `);
+
   return $tweet;
 };
 
+//Used both on the initial load of all tweets and to render new tweets as they are created
 const renderTweets = function(tweets) {
   for (const tweet of tweets) {
     const $tweet = createTweetElement(tweet);
+    //Prepend allows for reverse chronological order (vs append)
     $('#tweets-container').prepend($tweet);
   }
 };
 
 
 $(document).ready(function() {
-
+  //Initial retrieval of tweets on page load.
   loadTweets();
 
 });
