256337edf3323b89f715542b00b7d344d8256bcd
Zackory Kelly <zackkelly@live.ca>
2021-04-22 23:53:34 +0000

    Change char counter to reset to inherit instead of hard coded value for text color. Clean up tweet form code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,17 @@
+//This script adds a listener to the tweet form textbox in order to implement the 'remaining character counter'
 $(document).ready(function() {
   $("#tweet-text").on('input', function() {
     const remainingChars = 140 - $(this).val().length;
+    //Access the counter relative to 'this' rather than searching the entire document for it
     const counter = $(this).parent().siblings('.form-bottom').children('.counter')[0];
 
     counter.innerText = remainingChars;
 
+    //Sets color to red if the char counter is below 0, reverts it to the default otherwise
     if (remainingChars < 0) {
       $(counter).css('color', '#FF0000');
     } else {
-      $(counter).css('color', '#545149');
+      $(counter).css('color', 'inherit');
     }
 
   });
--- public/scripts/tweet-form.js
+++ public/scripts/tweet-form.js
@@ -1,21 +1,27 @@
-const loadTweet = function(tweet) {
+//Sends only the most recent tweet to be rendered, for use after new post
+const loadTweet = function() {
   $.get("/tweets", function(tweets) {
     renderTweets([tweets[tweets.length - 1]]);
   });
 };
 
+//Used to add a custom error message and then reveal the error message area
 const displayError = function(message) {
   $(".error-message").html(message);
   $(".error-message").slideDown();
 };
 
+
 $(document).ready(function() {
+  //Hide error message area upon any submission
   $(".error-message").hide();
+
   $('.new-form').on('submit', function(event) {
     $(".error-message").hide();
+    //Prevent normal button behaviour (including leaving the page)
     event.preventDefault();
+    //Retrieve raw message for the purposes ot checking message length
     const unserialized = $(this).find("textarea").val();
-    const message = $(this).serialize();
 
     if (unserialized.length <= 0) {
       displayError("⚠️ Error! Message field must not be empty! ⚠️");
@@ -25,7 +31,11 @@ $(document).ready(function() {
       return;
     }
 
+    //Serialized message is sent to the server to generate the tweet
+    const message = $(this).serialize();
+
     $.post("/tweets", message, function() {
+      //Reset the form to empty and load the new tweet upon submission
       $("#tweet-text").val("");
       loadTweet();
     });
